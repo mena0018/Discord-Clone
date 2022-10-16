d6df8f5b58850f9661ce225bf499ce130d4d7e55
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-10-16 18:01:34 +0200

    Implementation of the SideBar component

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,12 @@
+
+const SideBar = () => {
+  return (
+    <div className='fixed top-0 h-screen w-16 m-0 
+                    flex flex-col
+                  bg-gray-800 text-white shadow-lg'>
+
+    </div>
+  )
+}
+
+export default SideBar
