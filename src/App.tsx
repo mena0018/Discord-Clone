182736a47e14549fff67eca8d61b8279df107255
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-10-16 18:01:59 +0200

    Addition of the SideBar component into the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import SideBar from "./components/SideBar";
+
 function App() {
   return (
-    <div className="h-[100vh] bg-slate-900">
-      <h1 className="text-center text-4xl text-gray-100 p-40">Hello</h1>
+    <div className="flex">
+      <SideBar />
     </div>
   );
 }
